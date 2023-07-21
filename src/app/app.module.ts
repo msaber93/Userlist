dadcb3eb0b39a7fdbbbef82e15ff97f37914afe3
Mahmoud Saber <engabosaber2014@gmail.com>
2023-07-21 20:21:45 +0200

    fix(fe): remove unused fa icons module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,10 @@
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './core/navbar/navbar.component';
 import { TableComponent } from './table/table.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
@@ -18,7 +15,6 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule,
     HttpClientModule
   ],
   providers: [],
